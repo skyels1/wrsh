3a18b895365fef5f651b08ef4634ccc3f57b45d9
Skye <sk523858@dal.ca>
2026-02-03 02:13:38 -0400

    added external commands

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
-use std::io;
-use std::io::Write;
-use std::process::Command;
+use std::io; // obvious
+use std::io::Write; // this allows flush on stdout
+use std::process::Command; // used to take in commands
 
 fn main() {
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
+
         match io::stdin().read_line(&mut input) {
+
             Ok(0) => break,
             Ok(_) => {
-                if input.to_lowercase().trim().eq_ignore_ascii_case("q") {
+
+                let mut parts = input.trim().split_whitespace(); // create parts and split to get the command
+                let command = parts.next().unwrap_or("");
+
+                if command == "q" { // shell closing handling 
                     print!("closing shell...");
                     break;
                 }
+
+                match Command::new(command)
+                    .args(parts)
+                    .status() // supposedly better here than output to handle crashing?
+                {
+                    Ok(status) => {
+                        if !status.success() {
+                            println!("Proccess ended: {}", status);
+                        }
+                    }
+                    Err(e) => eprintln!("{}: {}", command, e) // error for reading commands
+                }
+
             },
-            Err(e) => println!("Error reading input: {}", e)
+            Err(e) => eprintln!("Error reading input: {}", e) // error message handler
         }
     }
 }
